8019a753d0d0c8141abc099c71a3ccdaef40b8d4
Cody <cody.wildtyto@gmail.com>
2024-05-10 23:09:37 -0400

    chore(general): update api endpoint

--- src/configs/variables.ts
+++ src/configs/variables.ts
@@ -1,14 +1,6 @@
-export const API_URL = 'https://youtube.googleapis.com/youtube/v3';
+export const API_URL = 'https://shiba-tv-api.vercel.app/';
 
 export const APP_AREA_PADDING = 40;
-
-export const DEFAULT_CHANNEL_IDS = [
-    'UCB5EDc_slf3zfKfQoOeitjQ',
-    'UCjlcOM4JaLoUbFWQ7j0LOKA',
-    'UC-xjitW_J39_Q1ure2HlJew',
-    'UCncfAuyvDvEd7K2dL7GlN3A',
-    'UCd-FQwyHwEh7U8kchB1FBnA',
-];
 
 export const MAIN_CHANNEL_ACTIVE_Y = 300;
 
